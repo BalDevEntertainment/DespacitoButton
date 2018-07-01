19433dc9edfc6fc5bd87791cadde52b4ff716a65
Ariel <baldeventertainment@gmail.com>
2018-07-01 14:17:13 -0300

    Removed unused images. Changed Wavs for MP3. Added Rate Button. Added texts in Pt and Es

--- app/src/main/java/com/baldeventertainment/despacitobutton/MainActivity.kt
+++ app/src/main/java/com/baldeventertainment/despacitobutton/MainActivity.kt
@@ -7,8 +7,8 @@ import android.support.v7.app.AppCompatActivity
 import com.google.android.gms.ads.*
 import kotlinx.android.synthetic.main.activity_main.*
 import com.google.firebase.analytics.FirebaseAnalytics
-
-
+import android.content.ActivityNotFoundException
+import android.net.Uri
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mInterstitialAd: InterstitialAd
@@ -29,6 +29,10 @@ class MainActivity : AppCompatActivity() {
 
         share.setOnClickListener {
             createShareIntent()
+        }
+
+        rate.setOnClickListener {
+            rateApp()
         }
 
         val des = MediaPlayer.create(this, R.raw.des)
@@ -70,7 +74,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun rateApp() {
+        mFirebaseAnalytics!!.logEvent("rate_app",  Bundle.EMPTY)
+
+        val uri = Uri.parse("market://details?id=com.baldeventertainment.despacitobutton")
+        val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or
+                Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+        try {
+            startActivity(goToMarket)
+        } catch (e: ActivityNotFoundException) {
+            startActivity(Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=com.baldeventertainment.despacitobutton")))
+        }
+
+
+    }
+
     private fun createShareIntent() {
+        mFirebaseAnalytics!!.logEvent(FirebaseAnalytics.Event.SHARE, Bundle.EMPTY)
+
         val shareIntent = Intent()
         shareIntent.action = Intent.ACTION_SEND
         shareIntent.type="text/plain"
